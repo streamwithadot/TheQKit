90a4831b468d6b91e38ed73d4d83aeb37d626933
Spohn <Spohn@stre.am>
2019-11-06 12:07:29 -0500

    Fixing example app to use Cocoapod and update firebase login

---
+++ Example/TheQKit/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  TheQKit_Example
+//
+//  Created by Jonathan Spohn on 11/6/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import TheQKit
+import FirebaseUI
+
+class LoginViewController: UIViewController {
+
+    var authUI : FUIAuth?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+                
+        let providers: [FUIAuthProvider] = [
+            FUIPhoneAuth(authUI:FUIAuth.defaultAuthUI()!)
+        ]
+        
+        self.authUI!.providers = providers
+
+        let phoneProvider = FUIAuth.defaultAuthUI()!.providers.first as! FUIPhoneAuth
+        phoneProvider.signIn(withPresenting: self, phoneNumber: nil)
+    }
+
+}
+
+
+// MARK: - FireBase UI
+extension LoginViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, url: URL?, error: Error?) {
+        if let user = authDataResult?.user {
+            
+            user.getIDTokenResult(completion: { (result, error) in
+                let token = result?.token
+                let userId:String = user.uid
+                
+                TheQKit.LoginQUserWithFirebase(userId: userId, tokenString: token!, username: "FirebaseTester") { (success) in
+                    if(success){
+                        self.performSegue(withIdentifier: "Onward!", sender: self)
+                    }else{
+                        print("Something went wrong")
+                    }
+                }
+            })
+        }
+    }
+}
